583719342ed6e560bc17790d43f39948b152980c
Claudio <claudiopereira@ipvc.pt>
2019-01-06 01:04:01 +0000

    even more stuff

--- src/Classes/Recinto.java
+++ src/Classes/Recinto.java
@@ -5,105 +5,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recinto implements Serializable {
-    
-    private List<Empresa> empresa;
-    private List<Desporto> desportos;
-    private List<Servicos> servicos;
-    private List<Horario> horarios;
-    private String nome;
-    private String morada;
-    private String localidade;
-    private TipoRecinto tipoRecinto;
-    private List<Reserva> reservas;
-    private boolean ativo;
 
-    public Recinto() {
-        this.desportos = new ArrayList<>();
-        this.servicos = new ArrayList<>();
-        this.horarios = new ArrayList<>();
-        this.reservas = new ArrayList<>();
-    }
+   private List<Desporto> desportos;
+   private List<Servicos> servicos;
+   private List<Horario> horarios;
+   private String nome;
+   private String morada;
+   private String localidade;
+   private TipoRecinto tipoRecinto;
+   private List<Reserva> reservas;
+   private boolean ativo;
 
-    public List<Empresa> getEmpresa() {
-        return empresa;
-    }
+   public Recinto() {
+      this.desportos = new ArrayList<>();
+      this.servicos = new ArrayList<>();
+      this.horarios = new ArrayList<>();
+      this.reservas = new ArrayList<>();
+   }
 
-    public void setEmpresa(List<Empresa> empresa) {
-        this.empresa = empresa;
-    }
+   public List<Desporto> getDesportos() {
+      return desportos;
+   }
 
-    public List<Desporto> getDesportos() {
-        return desportos;
-    }
+   public void setDesportos(List<Desporto> desportos) {
+      this.desportos = desportos;
+   }
 
-    public void setDesportos(List<Desporto> desportos) {
-        this.desportos = desportos;
-    }
+   public List<Servicos> getServicos() {
+      return servicos;
+   }
 
-    public List<Servicos> getServicos() {
-        return servicos;
-    }
+   public void setServicos(List<Servicos> servicos) {
+      this.servicos = servicos;
+   }
 
-    public void setServicos(List<Servicos> servicos) {
-        this.servicos = servicos;
-    }
+   public List<Horario> getHorarios() {
+      return horarios;
+   }
 
-    public List<Horario> getHorarios() {
-        return horarios;
-    }
+   public void setHorario(List<Horario> horarios) {
+      this.horarios = horarios;
+   }
 
-    public void setHorario(List<Horario> horarios) {
-        this.horarios = horarios;
-    }
+   public String getNome() {
+      return nome;
+   }
 
-    public String getNome() {
-        return nome;
-    }
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+   public String getMorada() {
+      return morada;
+   }
 
-    public String getMorada() {
-        return morada;
-    }
+   public void setMorada(String morada) {
+      this.morada = morada;
+   }
 
-    public void setMorada(String morada) {
-        this.morada = morada;
-    }
+   public String getLocalidade() {
+      return localidade;
+   }
 
-    public String getLocalidade() {
-        return localidade;
-    }
+   public void setLocalidade(String localidade) {
+      this.localidade = localidade;
+   }
 
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
+   public TipoRecinto getTipoRecinto() {
+      return tipoRecinto;
+   }
 
-    public TipoRecinto getTipoRecinto() {
-        return tipoRecinto;
-    }
+   public void setTipoRecinto(TipoRecinto tipoRecinto) {
+      this.tipoRecinto = tipoRecinto;
+   }
 
-    public void setTipoRecinto(TipoRecinto tipoRecinto) {
-        this.tipoRecinto = tipoRecinto;
-    }
+   public List<Reserva> getReservas() {
+      return reservas;
+   }
 
-    public List<Reserva> getReservas() {
-        return reservas;
-    }
+   public void setReservas(List<Reserva> reservas) {
+      this.reservas = reservas;
+   }
 
-    public void setReservas(List<Reserva> reservas) {
-        this.reservas = reservas;
-    }
-    
-    public boolean isAtivo() {
+   public boolean isAtivo() {
       return ativo;
    }
 
    public void setAtivo(boolean ativo) {
       this.ativo = ativo;
    }
-    
-    
-    
+
 }
